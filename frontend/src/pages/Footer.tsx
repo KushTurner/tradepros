f5985df27733ca7dd22c2e95fb868bcf35ccc513
KushTurner <kushturner9@gmail.com>
2023-07-24 17:02:27 +0100

    Added responsive homepage

---
+++ frontend/src/pages/Footer.tsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer className="bg-[#080808] my-6 text-sm">
+      <p className="font-display text-[#9395A5] text-center">
+        Made by <span className="text-[#5367FE]">Kush</span> and{' '}
+        <span className="text-[#5367FE]">Lyle</span>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
