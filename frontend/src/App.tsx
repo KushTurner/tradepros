e07f23f61476362a9b981dcf5284d91fe1c5f6a6
KushTurner <kushturner9@gmail.com>
2023-08-14 01:19:58 +0100

    Added basic sign in page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,26 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Market from './pages/Market';
 import Portfolio from './pages/Portfolio';
 import Watchlist from './pages/Watchlist';
+import SignIn from './pages/SignIn';
 
 function App() {
+  const location = useLocation();
+  const hideRoutes = ['/signin', '/register'];
+
+  const shouldHideRoutes = hideRoutes.includes(location.pathname);
   return (
     <>
-      <NavBar />
+      {!shouldHideRoutes && <NavBar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/market" element={<Market />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/watchlist" element={<Watchlist />} />
+        <Route path="/signin" element={<SignIn />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
