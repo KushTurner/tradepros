Fixing issues + changes made: - Weights for RNN hidden layer weren't being initialised (worse reproducibility in training) - Fixing possible data leakage by ensuring that model is only trained on sequences in the past and validated on sequences that it has not seen yet. (Generating batches in chronological order instead of randomly). - Added cuda manual seed - Data handler retrieves data from 6 company tickers from 2015 to 2020 (To align with the sentiment data).

ML/data_handler.py
@@ -16,6 +16,7 @@ from math import ceil as math_ceil
 from torch.cuda import memory_summary as torch_cuda_memory_summary
 from os import mkdir as os_mkdir
 from os.path import join as os_path_join
+from torch import arange as torch_arange
 
 class DataHandler:
 
@@ -184,21 +185,15 @@ class DataHandler:
 
         return torch_tensor(pandas_dataframe.values, dtype = desired_dtype)
 
-    def generate_batch(self, batch_size, dataset, num_context_days):
+    def generate_batch(self, batch_size, dataset, num_context_days, start_idx):
         
         # Find the inputs and labels in the dataset and find the number of examples in this set
         inputs, labels = dataset
-        num_examples = labels.shape[0]
-        
-        # Generate indexes which correspond to each example in the labels and inputs of this dataset (perform using CUDA if possible)
-        u_distrib = torch_ones(num_examples, device = self.device) / num_examples # Uniform distribution
-        example_idxs = torch_multinomial(input = u_distrib, num_samples = batch_size, replacement = True, generator = self.generator)
 
-        # Move indices to the "cpu" so that we can index the dataset, which is currently stored on the CPU
-        example_idxs = example_idxs.to(device = "cpu")
+        # Generate batch indexes starting from the start index, which correspond to each example in the labels and inputs of this dataset (perform using CUDA if possible)
+        example_idxs = [start_idx + idx for idx in range(batch_size)]
 
         if num_context_days == 1:
-
             # Return the examples and the corresponding targets (for predicting whether the price goes up or down for the next day)
             # Note: If self.device == "cuda", then the batch will be moved back onto the GPU
             return inputs[example_idxs].to(device = self.device), labels[example_idxs].to(device = self.device) 
@@ -707,7 +702,6 @@ class TextDataHandler:
         print([answer[:2] if answer[0] == "-" and answer[1] == "1" else answer[0] for answer in all_sentiments])
         print(len(all_sentiments))
 
-        # Merge the two datasets
         """
         Notes:
         - This essentially maps the corresponding sentiment scores based on the prompt (OR the tweet and ticker symbol), without needing to create a dictionary with hashed keys

ML/main.py
@@ -12,6 +12,7 @@ print(f"DEVICE | {DEVICE}")
 M_SEED = 2004
 # Seed for the model
 torch.manual_seed(M_SEED)
+torch.cuda.manual_seed_all(M_SEED)
 
 # Seed for data handler
 G = torch.Generator(device = DEVICE)
@@ -23,10 +24,18 @@ G.manual_seed(M_SEED)
 
 # Initialising data handler
 DH = DataHandler(device = DEVICE, generator = G)
+# DH.retrieve_data(
+#                 tickers = ["amzn", "ebay", "baba", "3690.HK"],
+#                 start_date = "7/07/1993",
+#                 end_date = "7/07/2023", 
+#                 interval = "1d",
+#                 transform_after = True
+#                 )
+
 DH.retrieve_data(
-                tickers = ["amzn", "ebay", "baba", "3690.HK"],
-                start_date = "7/07/2003",
-                end_date = "7/07/2023", 
+                tickers = ["aapl", "tsla", "amzn", "goog", "msft", "googl"],
+                start_date = "1/01/2015",
+                end_date = "31/12/2019", 
                 interval = "1d",
                 transform_after = True
                 )
@@ -54,13 +63,13 @@ DH.create_folds(num_folds = 10, N_OR_S = model.N_OR_S)
 TRAIN_FOLDS, VAL_FOLDS = DH.retrieve_k_folds(window_size = 2, N_OR_S = model.N_OR_S)
 
 # Testing generate_batch
-X1, Y1 = DH.generate_batch(batch_size = 5, dataset = TRAIN_FOLDS, num_context_days = num_context_days)
+X1, Y1 = DH.generate_batch(batch_size = 5, dataset = TRAIN_FOLDS, num_context_days = num_context_days, start_idx = 0)
 print(X1.shape, Y1.shape)
 
-X2, Y2 = DH.generate_batch(batch_size = 5, dataset = TRAIN_FOLDS, num_context_days = num_context_days)
+X2, Y2 = DH.generate_batch(batch_size = 5, dataset = TRAIN_FOLDS, num_context_days = num_context_days, start_idx = 0)
 print(X2.shape, Y2.shape)
 
-X3, Y3 = DH.generate_batch(batch_size = 5, dataset = TRAIN_FOLDS, num_context_days = num_context_days)
+X3, Y3 = DH.generate_batch(batch_size = 5, dataset = TRAIN_FOLDS, num_context_days = num_context_days, start_idx = 0)
 print(X3.shape, Y3.shape)
 
 # Training:
@@ -101,10 +110,21 @@ for k in range(num_sets):
     # - Window size starts at 2: Window sizes = (2 + 0), (2 + 1) (2 + 2) (2 + 3)
     # - Number of total sets = (num_folds - 1)
     TRAIN_FOLDS, VAL_FOLDS = DH.retrieve_k_folds(window_size = 2 + k, N_OR_S = model.N_OR_S)
+    
 
-    for i in range(EPOCHS):
+    # Rolling window variables: (Starting indexes, number of batches in each of the training / validation folds, interval for evaluating on the validation fold)
+    train_fold_sidx = 0
+    val_fold_sidx = 0
+    num_validations = VAL_FOLDS[1].shape[0] - BATCH_SIZE + 1
+    num_trains = TRAIN_FOLDS[1].shape[0] - BATCH_SIZE + 1
+    validation_interval = int(num_trains / num_validations)
+    print(num_trains, num_validations, validation_interval, num_trains / num_validations)
+    print(TRAIN_FOLDS[1].shape, VAL_FOLDS[1].shape)
+
+    for i in range(num_trains):
         # Generate inputs and labels
-        Xtr, Ytr = DH.generate_batch(batch_size = BATCH_SIZE, dataset = TRAIN_FOLDS, num_context_days = num_context_days)
+        Xtr, Ytr = DH.generate_batch(batch_size = BATCH_SIZE, dataset = TRAIN_FOLDS, num_context_days = num_context_days, start_idx = train_fold_sidx)
+        train_fold_sidx += 1
 
         # Forward pass
         logits = model(Xtr)
@@ -113,25 +133,33 @@ for k in range(num_sets):
         # Note: Did not use F.softmax and F.nll_loss because of floating point accuracy
         loss = F.cross_entropy(logits, Ytr)
 
-        with torch.no_grad():
+        # Evaluate the model
+        """
+        Notes:
+        - Trains on all of the training examples in the current training fold, evaluating on the validation set on a set interval, based on the ratio between the sizes of the training folds and validation fold
+        - If all of the validation examples have been used up and there are still training examples left, evaluation is not performed and the model will be trained with the remainder of the training examples until the next fold
+        """
+        if (i + 1) % validation_interval == 0 and val_fold_sidx < num_validations:
+            with torch.no_grad():
+                # Note: Must set to evaluation mode as BatchNorm layers and Dropout layers behave differently during training and evaluation
+                # BatchNorm layers - stops updating the moving averages in BatchNorm layers and uses running statistics instead of per-batch statistics
+                # Dropout layers - de-activated during evaluation
+                model.eval()
 
-            # Note: Must set to evaluation mode as BatchNorm layers and Dropout layers behave differently during training and evaluation
-            # BatchNorm layers - stops updating the moving averages in BatchNorm layers and uses running statistics instead of per-batch statistics
-            # Dropout layers - de-activated during evaluation
-            model.eval()
+                # Find the accuracy, precision, recall and f1 score on the training batch
+                preds = F.softmax(logits, dim = 1) # Softmax to find probability distribution
+                train_accuracy, train_precision, train_recall, train_f1 = find_P_A_R(predictions = preds, targets = Ytr)
+                
+                # Find the loss, accuracy, precision, recall and f1 score on a validation batch
+                Xva, Yva = DH.generate_batch(batch_size = BATCH_SIZE, dataset = VAL_FOLDS, num_context_days = num_context_days, start_idx = val_fold_sidx)
+                val_fold_sidx += 1
 
-            # Find the accuracy, precision, recall and f1 score on the training batch
-            preds = F.softmax(logits, dim = 1) # Softmax to find probability distribution
-            train_accuracy, train_precision, train_recall, train_f1 = find_P_A_R(predictions = preds, targets = Ytr)
-            
-            # Find the loss, accuracy, precision, recall and f1 score on a validation batch
-            Xva, Yva = DH.generate_batch(batch_size = BATCH_SIZE, dataset = VAL_FOLDS, num_context_days = num_context_days)
-            v_logits = model(Xva)
-            v_loss = F.cross_entropy(v_logits, Yva)
-            v_preds = F.softmax(v_logits, dim = 1)
-            val_accuracy, val_precision, val_recall, val_f1 = find_P_A_R(predictions = v_preds, targets = Yva)
+                v_logits = model(Xva)
+                v_loss = F.cross_entropy(v_logits, Yva)
+                v_preds = F.softmax(v_logits, dim = 1)
+                val_accuracy, val_precision, val_recall, val_f1 = find_P_A_R(predictions = v_preds, targets = Yva)
 
-            model.train()
+                model.train()
 
         # Backward pass
         optimiser.zero_grad()
@@ -158,26 +186,43 @@ for k in range(num_sets):
         train_f1_i.append(train_f1)
         val_f1_i.append(val_f1)
 
-        if i == 0 or (i + 1) % STAT_TRACK_INTERVAL == 0:
-            print(f"K: {k + 1}/{num_sets} | Epoch: {i + 1} | TLoss: {loss.item()} | VLoss: {v_loss.item()} | TAccuracy: {train_accuracy} | VAccuracy: {val_accuracy} | TPrecision: {train_precision} | VPrecision: {val_precision} | TRecall: {train_recall} | VRecall: {val_recall} | TF1 {train_f1} | VF1: {val_f1}")
+        if i == 0 or i - num_trains == 0 or (i + 1) % validation_interval == 0:
+            print(f"K: {k + 1}/{num_sets} | Epoch: {i + 1}/{num_trains} | TLoss: {loss.item()} | VLoss: {v_loss.item()} | TAccuracy: {train_accuracy} | VAccuracy: {val_accuracy} | TPrecision: {train_precision} | VPrecision: {val_precision} | TRecall: {train_recall} | VRecall: {val_recall} | TF1 {train_f1} | VF1: {val_f1}")
 
 
     # Record metrics for this fold:
-    # -EPOCHS: = Last EPOCHS items (i.e. all the statistics from this fold)
-    fold_t_losses.append((sum(train_loss_i[-EPOCHS:]) / EPOCHS))
-    fold_v_losses.append((sum(val_loss_i[-EPOCHS:]) / EPOCHS))
+    # -num_trains: = Last num_trains items (i.e. all the statistics from this fold)
+    fold_t_losses.append((sum(train_loss_i[-num_trains:]) / num_trains))
+    fold_v_losses.append((sum(val_loss_i[-num_trains:]) / num_trains))
 
-    fold_t_accuracies.append((sum(train_accuracy_i[-EPOCHS:]) / EPOCHS))
-    fold_v_accuracies.append((sum(val_accuracy_i[-EPOCHS:]) / EPOCHS))
+    fold_t_accuracies.append((sum(train_accuracy_i[-num_trains:]) / num_trains))
+    fold_v_accuracies.append((sum(val_accuracy_i[-num_trains:]) / num_trains))
 
-    fold_t_precisions.append((sum(train_precision_i[-EPOCHS:]) / EPOCHS))
-    fold_v_precisions.append((sum(val_precision_i[-EPOCHS:]) / EPOCHS))
+    fold_t_precisions.append((sum(train_precision_i[-num_trains:]) / num_trains))
+    fold_v_precisions.append((sum(val_precision_i[-num_trains:]) / num_trains))
 
-    fold_t_recalls.append((sum(train_recall_i[-EPOCHS:]) / EPOCHS))
-    fold_v_recalls.append((sum(val_recall_i[-EPOCHS:]) / EPOCHS))
+    fold_t_recalls.append((sum(train_recall_i[-num_trains:]) / num_trains))
+    fold_v_recalls.append((sum(val_recall_i[-num_trains:]) / num_trains))
 
-    fold_t_f1s.append((sum(train_f1_i[-EPOCHS:]) / EPOCHS))
-    fold_v_f1s.append((sum(val_f1_i[-EPOCHS:]) / EPOCHS))
+    fold_t_f1s.append((sum(train_f1_i[-num_trains:]) / num_trains))
+    fold_v_f1s.append((sum(val_f1_i[-num_trains:]) / num_trains))
+
+    # # Record metrics for this fold:
+    # # -EPOCHS: = Last EPOCHS items (i.e. all the statistics from this fold)
+    # fold_t_losses.append((sum(train_loss_i[-EPOCHS:]) / EPOCHS))
+    # fold_v_losses.append((sum(val_loss_i[-EPOCHS:]) / EPOCHS))
+
+    # fold_t_accuracies.append((sum(train_accuracy_i[-EPOCHS:]) / EPOCHS))
+    # fold_v_accuracies.append((sum(val_accuracy_i[-EPOCHS:]) / EPOCHS))
+
+    # fold_t_precisions.append((sum(train_precision_i[-EPOCHS:]) / EPOCHS))
+    # fold_v_precisions.append((sum(val_precision_i[-EPOCHS:]) / EPOCHS))
+
+    # fold_t_recalls.append((sum(train_recall_i[-EPOCHS:]) / EPOCHS))
+    # fold_v_recalls.append((sum(val_recall_i[-EPOCHS:]) / EPOCHS))
+
+    # fold_t_f1s.append((sum(train_f1_i[-EPOCHS:]) / EPOCHS))
+    # fold_v_f1s.append((sum(val_f1_i[-EPOCHS:]) / EPOCHS))
 
 # Set model to evaluation mode (For dropout + batch norm layers)
 model.eval()
@@ -205,68 +250,67 @@ print("-----------------------------------------------------------------")
 print("Loss during training")
 
 # Plotting train / validation loss
-A = 50
+total_epochs = len(train_loss_i)
+print(total_epochs)
+A = 84
 train_loss_i = torch.tensor(train_loss_i).view(-1, A).mean(1)
 val_loss_i = torch.tensor(val_loss_i).view(-1, A).mean(1)
 
-# train_loss_i = torch.tensor(torch.log10(train_loss_i)).view(-1, A).mean(1)
-# val_loss_i = torch.tensor(torch.log10(val_loss_i)).view(-1, A).mean(1)
-
 fig, ax = plt.subplots()
-ax.plot([i for i in range(int((EPOCHS * (num_sets)) / A))], train_loss_i, label = "Train")
-ax.plot([i for i in range(int((EPOCHS * (num_sets)) / A))], val_loss_i, label = "Validation")
-ax.legend()#
+ax.plot([i for i in range(int((total_epochs) / A))], train_loss_i, label = "Train")
+ax.plot([i for i in range(int((total_epochs) / A))], val_loss_i, label = "Validation")
+ax.legend()
 plt.show()
 
 print("-----------------------------------------------------------------")
 print("Accuracy during training")
 
-B = 100
+B = 98
 train_accuracy_i = torch.tensor(train_accuracy_i).view(-1, B).mean(1)
 val_accuracy_i = torch.tensor(val_accuracy_i).view(-1, B).mean(1)
 
 fig, ax = plt.subplots()
-ax.plot([i for i in range(int((EPOCHS * (num_sets)) / B))], train_accuracy_i, label = "Train")
-ax.plot([i for i in range(int((EPOCHS * (num_sets)) / B))], val_accuracy_i, label = "Validation")
+ax.plot([i for i in range(int((total_epochs) / B))], train_accuracy_i, label = "Train")
+ax.plot([i for i in range(int((total_epochs) / B))], val_accuracy_i, label = "Validation")
 ax.legend()
 plt.show()
 
 print("-----------------------------------------------------------------")
 print("Precision during training")
 
-C = 100
+C = 98
 train_precision_i = torch.tensor(train_precision_i).view(-1, C).mean(1)
 val_precision_i = torch.tensor(val_precision_i).view(-1, C).mean(1)
 
 fig, ax = plt.subplots()
-ax.plot([i for i in range(int((EPOCHS * (num_sets)) / C))], train_precision_i, label = "Train")
-ax.plot([i for i in range(int((EPOCHS * (num_sets)) / C))], val_precision_i, label = "Validation")
+ax.plot([i for i in range(int((total_epochs) / C))], train_precision_i, label = "Train")
+ax.plot([i for i in range(int((total_epochs) / C))], val_precision_i, label = "Validation")
 ax.legend()
 plt.show()
 
 print("-----------------------------------------------------------------")
 print("Recall during training")
 
-D = 100
+D = 98
 train_recall_i = torch.tensor(train_recall_i).view(-1, D).mean(1)
 val_recall_i = torch.tensor(val_recall_i).view(-1, D).mean(1)
 
 fig, ax = plt.subplots()
-ax.plot([i for i in range(int((EPOCHS * (num_sets)) / D))], train_recall_i, label = "Train")
-ax.plot([i for i in range(int((EPOCHS * (num_sets)) / D))], val_recall_i, label = "Validation")
+ax.plot([i for i in range(int((total_epochs) / D))], train_recall_i, label = "Train")
+ax.plot([i for i in range(int((total_epochs) / D))], val_recall_i, label = "Validation")
 ax.legend()
 plt.show()
 
 print("-----------------------------------------------------------------")
 print("F1 score during training")
 
-E = 100
+E = 98
 train_f1_i = torch.tensor(train_f1_i).view(-1, E).mean(1)
 val_f1_i = torch.tensor(val_f1_i).view(-1, E).mean(1)
 
 fig, ax = plt.subplots()
-ax.plot([i for i in range(int((EPOCHS * (num_sets)) / E))], train_f1_i, label = "Train")
-ax.plot([i for i in range(int((EPOCHS * (num_sets)) / E))], val_f1_i, label = "Validation")
+ax.plot([i for i in range(int((total_epochs) / E))], train_f1_i, label = "Train")
+ax.plot([i for i in range(int((total_epochs) / E))], val_f1_i, label = "Validation")
 ax.legend()
 plt.show()
 